15e79a4ba85f7e5b3a588b5f511d9bb11030d5fa
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-06 10:39:11 +0900

    userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :type
-  validates :type_id, numericality: { other_than: 0 } 
+  validates :password,          format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i, message: 'は半角英数字どちらも含む8文字以上で入力してください' }
+  with_options presence: true do
+    validates :nickname
+    validates :type_id,   numericality: { other_than: 0, message: 'を選択してください' }
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
+    validates :birth_date
+  end
 end
