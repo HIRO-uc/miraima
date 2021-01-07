b40cca394afd55aef97a05a7bd5feb95803cf61e
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-07 10:21:20 +0900

    purofileの編集機能の実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,31 @@
 class ProfilesController < ApplicationController
+  before_action :current_path_profile, only: [:edit, :update]
+
   def new
     @user = User.find(params[:user_id])
+    @profile = Profile.new
   end
 
   def create
-    
+    @profile = Profile.new(profile_params)
+    @profile.save
+    redirect_to user_path(@profile.user_id)
+  end
+
+  def edit
+  end
+
+  def update
+    @profile.update(profile_params)
+    redirect_to user_path(@profile.user_id)
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:profile, :website).merge(user_id: params[:user_id])
+  end
+
+  def current_path_profile
+    @profile = Profile.find(params[:id])
   end
 end
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -2,6 +2,7 @@ class TipsController < ApplicationController
   def index
     if user_signed_in?
       @user = User.find(current_user.id)
+      @profiles = Profile.new
     end
   end
 end
