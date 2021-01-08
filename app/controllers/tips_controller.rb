49761b19719f7c0c13c6e196253ac48fd338d750
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-08 21:59:22 +0900

    tips投稿機能と削除機能の実装

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,5 +1,45 @@
 class TipsController < ApplicationController
+  before_action :current_path_tip, only: [:show, :destroy]
+  before_action :move_to_index, only: [:destroy]
+
   def index
-    
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      #@profiles = Profile.new
+    end
+    @tips = Tip.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @tip = Tip.new
+  end
+
+  def create
+    @tip = Tip.new(tip_params)
+    @tip.save
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  def destroy
+    @tip.destroy
+    redirect_to root_path
+  end
+
+  private
+  def tip_params
+    params.require(:tip).permit(:text).merge(user_id: current_user.id)
+  end
+
+  def current_path_tip
+    @tip = Tip.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && (@tip.user_id == current_user.id)
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
-  root to: 'home#index'
-  resources :tips, only: [:index]
+  root to: 'tips#index'
+  resources :tips, only: [:create, :new, :show, :destroy]
   resources :users, only: [:show] do
     resources :profiles, only: [:new, :create, :edit, :update]
     resources :careers,  only: [:new, :create, :edit, :update]
