7021bf8a894159e02e0aeab2cb2d950371cc4ce9
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-10 08:28:59 +0900

    自分以外のユーザー情報編集の制限

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,5 +1,7 @@
 class CareersController < ApplicationController
+  before_action :current_path_user, only: [:new, :create, :edit, :update, :move_to_index]
   before_action :current_path_career, only: [:edit, :update]
+  before_action :move_to_index, only: [:new, :create, :edit, :update]
 
   def new
     @user = User.find(params[:user_id])
@@ -25,7 +27,17 @@ class CareersController < ApplicationController
     params.require(:career).permit(:worthwhile, :ideal_candidate, :can_do_now, :recommended_book).merge(user_id: params[:user_id])
   end
 
+  def current_path_user
+    @user = User.find(params[:user_id])
+  end
+
   def current_path_career
     @career = Career.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && (current_user.id == @user.id)
+      redirect_to root_path
+    end 
   end
 end
