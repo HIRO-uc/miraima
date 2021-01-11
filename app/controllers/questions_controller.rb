0ecadf7a010b2503f9f7d76fd729350345d9b85a
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-11 16:15:53 +0900

    ログアウト状態のユーザーのアクセス制限

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,9 @@
 class QuestionsController < ApplicationController
-  before_action :current_path_question, only: [:edit, :update, :show, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :current_path_question, only: [:edit, :update, :show, :destroy, :move_to_index]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
-    if user_signed_in?
-      @user = User.find(current_user.id)
-    end
     @questions = Question.includes(:user).order(created_at: :desc)
   end
 
@@ -35,12 +34,17 @@ class QuestionsController < ApplicationController
   end
 
   private
-
   def question_params
     params.require(:question).permit(:text, :title).merge(user_id: current_user.id)
   end
 
   def current_path_question
     @question = Question.find(params[:id])
+  end
+
+  def move_to_index
+    unless @question.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
