e046b3a85da8e32b25102152300144d7390afd4b
HIRO-uc <hiro.nyanta.mech@gmail.com>
2021-01-11 16:25:20 +0900

    careersコントローラーの銃声

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,10 +1,10 @@
 class CareersController < ApplicationController
+  before_action :authenticate_user!
   before_action :current_path_user, only: [:new, :create, :edit, :update, :move_to_index]
   before_action :current_path_career, only: [:edit, :update]
   before_action :move_to_index, only: [:new, :create, :edit, :update]
 
   def new
-    @user = User.find(params[:user_id])
     @career = Career.new
   end
 
@@ -36,7 +36,7 @@ class CareersController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && (current_user.id == @user.id)
+    unless current_user.id == @user.id
       redirect_to root_path
     end 
   end
